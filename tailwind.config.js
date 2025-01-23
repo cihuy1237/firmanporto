43202f65f83fbb6aadb91c837d9baefc194a346a
cihuy1237 <fynarata@gmail.com>
2025-01-22 18:23:05 -0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        yellow: {
+          400: '#FFD700',
+          300: '#FFE44D'
+        }
+      },
+    },
+  },
+  plugins: [],
+};
